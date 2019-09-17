f1079a4d4d7b6de6c357b41c73a8bb010781e6c2
Jason Prince <jasoncprince@gmail.com>
2019-09-17 12:04:56 -0600

    Added sessionConfig to server.js

--- server.js
+++ server.js
@@ -5,9 +5,29 @@ const UsersRouter = require("./users/users-router.js");
 
 const server = express();
 
+const sessionConfig = {
+  name: "chocochip", // would name cookie sid by default
+  secret: process.env.SESSION_SECRET || "kee it secret, keep it safe",
+  cookie: {
+    maxAge: 1000 * 60 * 60, // in milliseconds
+    secure: false, // true means only send cookie over https
+    httpOnly: true // true means JS has no access to the cookie
+  },
+  resave: false,
+  saveUninitialized: true, // GDPR compliance
+  store: new knexSessionStore({
+    knex: dbConnection,
+    tablename: "knexsessions",
+    sidfieldname: "sessionid",
+    createtable: true,
+    clearInterval: 1000 * 60 * 30 // clean out expired session data
+  })
+};
+
 server.use(helmet());
 server.use(express.json());
 server.use(cors());
+server.use(session(sessionConfig));
 server.use("/api", UsersRouter);
 
 module.exports = server;
